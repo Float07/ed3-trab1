cec0f7d001358381c48883f096523436fe4212b6
Thiago Daniel <thiagodaniel@pop-os.localdomain>
2021-10-12 20:38:08 -0300

    começo funcionalidade 2

--- main.c
+++ main.c
@@ -4,16 +4,7 @@
 //Tamanho máximo dos nomes dos arquivos de input e output
 #define MAX_FILENAME_SIZE 256
 
-int main(int argc, char *argv[]) {
-    int funcionalidade; //Irá registrar a funcionalidade escolhida pelo usuário
-
-    scanf("%d", &funcionalidade);
-
-    switch (funcionalidade)
-    {
-    //Leitura de um csv e escrita no binário
-    //Ao finalizar roda a função binarioNaTela()
-    case 1:;
+void funcionalidade1() {
         char inputFileName[MAX_FILENAME_SIZE];
         char outputFileName[MAX_FILENAME_SIZE];
         scanf("%s %s", inputFileName, outputFileName);
@@ -28,8 +19,32 @@ int main(int argc, char *argv[]) {
         
         binarioNaTela(outputFileName);
 
+        return;
+}
+
+void funcionalidade2() {
+        char inputFileName[MAX_FILENAME_SIZE];
+        scanf("%s", inputFileName);
+}
+
+int main(int argc, char *argv[]) {
+    int funcionalidade; //Irá registrar a funcionalidade escolhida pelo usuário
+
+    scanf("%d", &funcionalidade);
+
+    switch (funcionalidade)
+    {
+    //Leitura de um csv e escrita no binário
+    //Ao finalizar roda a função binarioNaTela()
+    case 1:
+        funcionalidade1();
         break;
     
+    //Imprime todos os dados de um arquivo binário
+    case 2:
+        funcionalidade2();
+        break;
+
     default:
         break;
     }
