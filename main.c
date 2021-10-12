7e72fde7397d3c639254383bef9d77c671811423
Thiago Daniel <thiagodaniel@pop-os.localdomain>
2021-10-12 18:44:40 -0300

    deletado código comentado

--- main.c
+++ main.c
@@ -5,20 +5,6 @@
 #define MAX_FILENAME_SIZE 256
 
 int main(int argc, char *argv[]) {
-    //FILE* csvInput = fopen("estacoes.csv", "r");
-    //FILE* binOutput = fopen("binario.bin", "wb");
-
-    //readCSV(csvInput, binOutput);
-
-    //fclose(csvInput);
-    //fclose(binOutput);
-
-    //FILE* binInput = fopen("binario.bin", "rb");
-
-    //printBin(binInput);
-
-    //fclose(binInput);
-
     int funcionalidade; //Irá registrar a funcionalidade escolhida pelo usuário
 
     scanf("%d", &funcionalidade);
